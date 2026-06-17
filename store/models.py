3b02f7d83cdc4facab627cbb32877a0c881caf6e
Liyan <lianneramosbiscocho@gmail.com>
2026-06-17 22:21:59 +0800

    Add files via upload

---
+++ store/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+
+    def product_info(self):
+        return f"{self.name} costs {self.price}"
+
